1c4db2a950b2f68446377e0bb72c878584de3cac
filipemartins999 <filipe@opportunitynetwork.com>
2020-12-22 22:38:08 +0100

    Parent Component (forgot to push this a couple of commits ago)

---
+++ src/components/counters.jsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import React, { Component, useState } from "react";
+import Counter from "./counter";
+
+function Counters() {
+  const [counters, setCounters] = useState([
+    { id: 1, value: 4 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+    { id: 4, value: 0 },
+  ]);
+
+  return (
+    <div>
+      {counters.map((counter) => (
+        <Counter key={counter.id} value={counter.value} id={counter.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Counters;
