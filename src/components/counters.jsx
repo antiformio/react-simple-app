3a39461d756384d48fa6d9517bde0bed181b6a7b
filipemartins999 <filipe@opportunitynetwork.com>
2020-12-23 21:17:17 +0100

    Added show or hide Reset button depending on wether there are any counters

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Counter from "./counter";
 import "../Counters.css";
 
@@ -10,14 +10,29 @@ function Counters({
   onReset,
   onAdd,
 }) {
+  const [showHideReset, setShowHideReset] = useState(false);
+
+  // Show Hide the reset button depending of the existence of counters
+  useEffect(() => {
+    if (counters.length > 0) setShowHideReset(true);
+    else setShowHideReset(false);
+  });
+
   return (
     <div className="counters">
-      <button onClick={() => onReset()} className="btn btn-primary btn-sm m-2">
-        Reset
-      </button>
       <button onClick={() => onAdd()} className="btn btn-success btn-sm">
         Add Counter
       </button>
+
+      {showHideReset && (
+        <button
+          onClick={() => onReset()}
+          className="btn btn-primary btn-sm ml-2"
+        >
+          Reset
+        </button>
+      )}
+
       {counters.map((counter) => (
         <Counter
           key={counter.id}
